09a395ff8f70d198517004e1b47efdbc6af919d4
Nagasupreeth Ramesh <ngsupreeth4@gmail.com>
2023-06-16 16:38:36 -0400

    refactored test cases

--- JavaHttpParser/src/com/broadcom/parser/DriverHttpParser.java
+++ JavaHttpParser/src/com/broadcom/parser/DriverHttpParser.java
@@ -9,15 +9,11 @@ public class DriverHttpParser {
 		JavaHttpParser obj = new JavaHttpParser();
 		List<String[]> list = new ArrayList<>();
 		String[] array1 = {
-				"HTTP/1.0 200 OK\r\n cache-control: public\r\n content-length: 0\r\n content-type: image/svg+xml\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n" };
-		String[] array2 = {
-				"HTTP/1.1 302 Found\r\n cache-control: public\r\n Transfer-encoding: chunked\r\n invalid_header\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n" };
-
-		String[] array3 = { "Header1: value1\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n content-length: 1337\r\n" };
+				"HTTP/1.0 200 OK\r\n cache-control: public\r\n content-length: 0\r\n content-type: image/svg+xml\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n",
+				"HTTP/1.1 302 Found\r\n cache-control: public\r\n Transfer-encoding: chunked\r\n invalid_header\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n",
+				"Header1: value1\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n content-length: 1337\r\n" };
 
 		list.add(array1);
-		list.add(array2);
-		list.add(array3);
 
 		System.out.println(obj.parseRequest(list));
 
