31023ffcc8a4e37a96361773a9a6b46a3d3818aa
Nagasupreeth Ramesh <ngsupreeth4@gmail.com>
2023-06-16 16:41:47 -0400

    renamed Input

--- JavaHttpParser/src/com/broadcom/parser/DriverHttpParser.java
+++ JavaHttpParser/src/com/broadcom/parser/DriverHttpParser.java
@@ -8,12 +8,12 @@ public class DriverHttpParser {
 	public static void main(String[] args) throws IOException, Exception {
 		JavaHttpParser obj = new JavaHttpParser();
 		List<String[]> list = new ArrayList<>();
-		String[] array1 = {
+		String[] httpResponse = {
 				"HTTP/1.0 200 OK\r\n cache-control: public\r\n content-length: 0\r\n content-type: image/svg+xml\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n",
 				"HTTP/1.1 302 Found\r\n cache-control: public\r\n Transfer-encoding: chunked\r\n invalid_header\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n",
 				"Header1: value1\r\n date: Tue, 22 Jun 2021 22:24:42 GMT\r\n content-length: 1337\r\n" };
 
-		list.add(array1);
+		list.add(httpResponse);
 
 		System.out.println(obj.parseRequest(list));
 
